04839eaa38afcc4f2c1900e602067091a7cb6201
Fabrice Geib <fabricegeib@gmail.com>
2022-03-08 13:37:04 +0100

    error await

--- my-wave-portal-app/src/App.js
+++ my-wave-portal-app/src/App.js
@@ -10,16 +10,64 @@ const wave = () => {
 
 // function App() {
 const App = () => {
-  const checkIfWalletIsConnected = () => {
-    /*
-    * First make sure we have access to window.ethereum
-    */
-    const { ethereum } = window;
+  /*
+  * Just a state variable we use to store our user's public wallet.
+  */
+  const [currentAccount, setCurrentAccount] = useState("");
 
-    if (!ethereum) {
-      console.log("Make sure you have metamask!");
-    } else {
-      console.log("We have the ethereum object", ethereum);
+  const checkIfWalletIsConnected = () => {
+    try {
+      /*
+      * First make sure we have access to window.ethereum
+      */
+      const { ethereum } = window;
+
+      // if (!ethereum) {
+      //   console.log("Make sure you have metamask!");
+      // } else {
+      //   console.log("We have the ethereum object", ethereum);
+      // }
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found")
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  /**
+  * Implement your connectWallet method here
+  */
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
     }
   }
 
@@ -52,9 +100,19 @@ const App = () => {
         </div>
         <div>A french web developer</div>
         <div>Connect your Ethereum wallet and wave at me !</div>
-        <button className="waveButton" onClick={wave}>
+        {/* <button className="waveButton" onClick={wave}> */}
+        <button className="waveButton" onClick={null}>
           Wave at Me
         </button>
+
+        {/*
+        * If there is no currentAccount render this button
+        */}
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
       </main>
     </div>
   );
